32e233657fe98013892d04327068330e695c3d00
rsookram <rashadsookram@gmail.com>
2021-10-03 13:46:50 -0400

    Fix test and logs

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -1,8 +1,13 @@
 import fs = require('fs')
+import os = require('os')
+import path = require('path')
 import * as installer from '../src/installer'
 import {expect, test} from '@jest/globals'
 
 test('acquires ktfmt', async () => {
-  const path = await installer.getKtfmt()
-  expect(fs.existsSync(path)).toBe(true)
+  const dir = fs.mkdtempSync(path.join(os.tmpdir(), 'ktfmt-'))
+  process.env['RUNNER_TEMP'] = dir
+
+  const ktfmt = await installer.getKtfmt()
+  expect(fs.existsSync(ktfmt)).toBe(true)
 }, 100000)
--- src/installer.ts
+++ src/installer.ts
@@ -8,10 +8,10 @@ export async function getKtfmt(): Promise<string> {
   const version = '0.28'
   const url = `https://search.maven.org/remotecontent?filepath=com/facebook/ktfmt/${version}/ktfmt-${version}-jar-with-dependencies.jar`
 
-  core.info(`Downloading $url`)
+  core.info(`Downloading ${url}`)
   // TODO: Try caching the file
   const path = tc.downloadTool(url)
-  core.debug(`Downloaded to $path`)
+  core.debug(`Downloaded to ${path}`)
 
   return path
 }
