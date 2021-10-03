197a4ecb83765f5fd356ad81419fae4bc0428be3
rsookram <rashadsookram@gmail.com>
2021-10-03 12:57:55 -0400

    Run ktfmt

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 import {getKtfmt} from './installer'
 
 async function run(): Promise<void> {
@@ -6,11 +7,11 @@ async function run(): Promise<void> {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
     const jar = await getKtfmt()
-    core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    // TODO: Accept style as an argument to the action
+    // TODO: Allow files / directions to be specified too
+    await exec.exec('java', ['-jar', jar, '--kotlinlang-style', '.'])
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
